ac4769927e8cb54752e287bd47b188099fcc664c
Paolo Rapuano <bettercallpaul07@gmail.com>
2022-12-19 15:06:57 +0100

    Little Fix

--- js/scripts.js
+++ js/scripts.js
@@ -8,25 +8,25 @@ let savedprice = 0;
 
 if (age < 18 ) {
     newcpk = cpk*0.80;
-    discount = "Complimenti, hai diritto ad uno sconto del 20% sul prezzo del biglietto;"
+    discount = "Complimenti, essendo minorenne hai diritto ad uno sconto del 20% sul prezzo del biglietto;"
     savedprice = cpk*0.20;
     savedprice = savedprice.toFixed(2) + '€'
     cpk = cpk + '€'
 
-    document.getElementById("nodiscountprice").innerHTML ="Prezzo del biglietto senza sconto: " + cpk;
-    document.getElementById("savedprice").innerHTML = discount + " hai risparmiato: " + savedprice;
+    document.getElementById("nodiscountprice").innerHTML ="Prezzo del biglietto senza sconto: " + cpk + '.';
+    document.getElementById("savedprice").innerHTML = discount + " hai risparmiato: " + savedprice + '.';
 
 }
 
 else if  (age >= 65) {
     newcpk = cpk*0.60;
-    discount = "Complimenti, hai diritto ad uno sconto del 40% sul prezzo del biglietto;"
+    discount = "Complimenti, essendo over 65 hai diritto ad uno sconto del 40% sul prezzo del biglietto;"
     savedprice = cpk*0.40;
     savedprice = savedprice.toFixed(2) + '€'
     cpk = cpk + '€'
 
-    document.getElementById("nodiscountprice").innerHTML ="Prezzo del biglietto senza sconto: " + cpk;
-    document.getElementById("savedprice").innerHTML = discount + " hai risparmiato: " + savedprice;
+    document.getElementById("nodiscountprice").innerHTML ="Prezzo del biglietto senza sconto: " + cpk + '.';
+    document.getElementById("savedprice").innerHTML = discount + " hai risparmiato: " + savedprice + '.';
 }
 
 else {
